f33875c8a407e4b2353b0d1cda4378be10e401ed
kdrzclk <kadir.ozcelik16@gmail.com>
2022-03-15 08:38:10 +0300

    ilk fonksiyon içerisinde yeni htmller oluşturuldu

--- app.js
+++ app.js
@@ -16,4 +16,49 @@ const renderCountry = (data, type = "country") => {
     name: { common: countryName },
     flags: { svg: flag },
   } = data;
+
+  const countryHtmlCard = `
+  <img src="${flag}" class="card-img-top border border-secondary" alt="Flag" />
+      <div class="card-body">
+        <h5 class="card-title">${countryName}</h5>
+        <p class="card-text">${region}</p>
+      </div>
+      <ul class="list-group list-group-flush">
+        <li class="list-group-item"><span><i class="fas fa-2x fa-landmark"></i>${capital}</span></li>
+        <li class="list-group-item"><span><i class="fas fa-lg fa-users"></i>${(
+          population / 1_000_000
+        ).toFixed(2)} M</span></li>
+        <li class="list-group-item"><span><i class="fas fa-lg fa-comments"></i>${Object.values(
+          languages
+        )}</span></li>
+        <li class="list-group-item">
+          <span
+            ><i class="fas fa-lg fa-money-bill-wave"></i>${
+              Object.values(currencies)[0].name
+            } ${Object.values(currencies)[0].symbol}
+            </span
+          >
+        </li>
+      </ul>
+  `;
+
+  if (type === "country") {
+    const countryHtml = `
+      <div class="container country">
+        <div class="row justify-content-center mt-5">
+          <div class="card country-card col col-sm-6 col-lg-3 py-3" >
+                ${countryHtmlCard}
+          </div>
+        </div>
+        <div class="row justify-content-start neighbour-container">
+        </div>
+      `;
+    const main = document.querySelector("main");
+    main.insertAdjacentHTML("afterbegin", countryHtml);
+  } else if (type === "neighbour") {
+    const neighbourHtml = `<div class="card col col-sm-6 col-lg-3 py-3 neighbour">${countryHtmlCard}</div>`;
+
+    const neighbourDiv = document.querySelectorAll(".neighbour-container");
+    neighbourDiv[0].insertAdjacentHTML("beforeend", neighbourHtml);
+  }
 };
